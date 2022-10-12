c2b5b5ce178bd3ba22721ed49b2cfda03c49f58c
silence-GPU <73737420+silence-GPU@users.noreply.github.com>
2022-10-12 22:35:49 +0800

    test version-1.0

--- src/main/java/com/jiejiezhuzhu/Application.java
+++ src/main/java/com/jiejiezhuzhu/Application.java
@@ -3,7 +3,7 @@ package com.jiejiezhuzhu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-
+//test
 @SpringBootApplication
 @ServletComponentScan
 public class Application {
